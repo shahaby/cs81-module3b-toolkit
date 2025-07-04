ba1046164a914b89bac51085535cf1a42820ceed
Shahab Yarmohammadi <yarmohammadi@gmail.com>
2025-07-04 09:17:08 -0700

    Add square func

--- math.js
+++ math.js
@@ -1,8 +1,16 @@
-// Function to double a number.
+// Function to double a number
 function double(n) {
     return n * 2;
 }
 
+// Function to square a number
+function square(n) {
+    return n * n;
+}
 
-// Test
+
+
+
+// Tests
 // console.log(double(4)); // Expect: 8
+// console.log(square(3)); // Expect: 9
