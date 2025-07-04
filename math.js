ae47f7259011ce4ac953ad787d4a0782c4b4adc8
Shahab Yarmohammadi <yarmohammadi@gmail.com>
2025-07-04 09:19:10 -0700

    Add multiply func

--- math.js
+++ math.js
@@ -18,10 +18,14 @@ function isOdd(n) {
     return n % 2 !== 0;
 }
 
-
+// Function to multiply two numbers
+function multiply(a, b) {
+    return a * b;
+}
 
 // Tests
 // console.log(double(4)); // Expect: 8
 // console.log(square(3)); // Expect: 9
 // console.log(isEven(10)); // Expect: true
 // console.log(isOdd(7)); // Expect: true
+// console.log(multiply(5, 6)); // Expect: 30
